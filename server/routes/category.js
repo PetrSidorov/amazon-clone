7180e4502a2407397c048a4c73e4642525a15aaa
Peter <piterpatrikk@gmail.com>
2021-08-25 00:02:43 +0300

    APIs for app

---
+++ server/routes/category.js
@@ -0,0 +1,42 @@
+const router = require('express').Router();
+const Category = require('../models/category');
+
+// post request
+router.post('/categories', async(req, res) =>{
+    try {
+        const category = new Category();
+        category.type = req.body.type;
+
+        await category.save();
+
+        res.json({
+            success: true,
+            message: 'New category created!'
+        });
+
+    } catch(err) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    }
+});
+
+module.exports = router
+
+//get request
+router.get('/categories', async (req, res) => {
+    try {
+        let categories = await Category.find();
+        res.json({
+            success: true,
+            categories: categories
+        });
+
+    } catch (err){
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    }
+});
--- server/routes/product.js
+++ server/routes/product.js
@@ -1,25 +1,22 @@
 const router = require('express').Router();
 const Product = require('../models/product');
+
+const upload = require('../middlewares/upload-photo');
+
 // post request - new product
-// category: { type: Schema.Types.ObjectId, ref: 'Category' },
-// owner: { type: Schema.Types.ObjectId, ref: 'Owner' },
-// title: String,
-// description: String,
-// photo: String,
-// price: Number,
-// stockQuantity: Number,
-// rating: [ Number ],
-router.post('/products', async(req, res) => {
+
+router.post('/products', upload.single('photo'), async(req, res) => {
     try {
         let product = new Product();
         product.title = req.body.title;
         product.description = req.body.description;
-        product.photo = req.body.photo;
+        product.photo = req.file.location;
         product.price = req.body.price;
         product.stockQuantity = req.body.stockQuantity;
         await product.save();
+
         res.json({
-            status: true,
+            success: true,
             message: 'Success save product',
         });
     } catch (err) {
@@ -30,12 +27,93 @@ router.post('/products', async(req, res) => {
     }
 
 });
+
 // get request - get all products
+
+router.get('/products', async (req, res) => {
+    try {
+
+        let products = await Product.find();
+        res.json({
+            products: products,
+            success: true,
+        })
+
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    }
+})
 
 // get request - get single product
 
+
+router.get('/products/:id', async (req, res) => {
+    try {
+        let product = await Product.findOne({ _id: req.params.id });
+        res.json({
+            product: product,
+            success: true,
+        })
+
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    }
+});
+
 // put request - update product
 
+router.put('/products/:id', upload.single('photo'), async (req, res) => {
+    try {
+        let product = await Product.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+            $set: {
+                title: req.body.title,
+                price: req.body.price,
+                category: req.body.category,
+                photo: req.body.photo,
+                description: req.body.description,
+                owner: req.body.ownerID,
+            }
+        },
+        { upsert: true }
+        );
+        res.json({
+            updatedProduct: product,
+            success: true,
+        })
+
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    }
+});
 // delete request - delete single product
+
+router.delete('/products/:id', async (req, res) => {
+    try {
+        let deletedProduct = await Product.findOneAndDelete({ _id: req.params.id });
+        if (deletedProduct) {
+            res.json({
+                status: true,
+                message: 'Success delete'
+            })
+        }
+
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    }
+})
 
 module.exports = router;
